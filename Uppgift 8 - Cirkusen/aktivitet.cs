780837f76ea8236a9844009114546330aa2e96f7
Johan Conradsson <conradsson1993@hotmail.com>
2016-05-30 12:17:15 +0200

    nytt

---
+++ Uppgift 8 - Cirkusen/aktivitet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift_8___Cirkusen
+{
+    public class aktivitet
+    {
+        public int aktivitetsid { get; set; } 
+        public int förnamn { get; set; }     // HÅLLER PÅ
+        public string efternamn { get; set; }
+
+        public string NamnDisplay
+        {
+            get
+            {
+                return aktivitetsid + " - " + förnamn + " " + efternamn;
+            }
+        }
+    }
+}
